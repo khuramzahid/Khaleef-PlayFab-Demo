683035f553f2ae0103d5bb4f3d8e9c8480ded301
Khuram Zahid <khuram.zahid@gmail.com>
2023-02-17 17:30:45 +0500

    Updates

--- index.js
+++ index.js
@@ -2,39 +2,36 @@ var playfab = require('playfab-sdk')
 var PlayFab = playfab.PlayFab
 var PlayFabClient = playfab.PlayFabClient
 
-function DoExampleLoginWithCustomID() {
-    PlayFab.settings.titleId = "A68E3";
-    var loginRequest = {
-        // Currently, you need to look up the correct format for this object in the API-docs:
-        // https://api.playfab.com/Documentation/Client/method/LoginWithCustomID
-        TitleId: PlayFab.settings.titleId,
-        CustomId: "GettingStartedGuide",
-        CreateAccount: true
-    };
+PlayFab.settings.titleId = "A68E3";
+var player1LoginRequest = {
+    TitleId: PlayFab.settings.titleId,
+    CustomId: "Player 1",
+    CreateAccount: true
+};
+var player2LoginRequest = {
+    TitleId: PlayFab.settings.titleId,
+    CustomId: "Player 2",
+    CreateAccount: true
+};
 
-    PlayFabClient.LoginWithCustomID(loginRequest, LoginCallback);
-}
-
-function LoginCallback(error, result) {
-    if (result !== null) {
-        console.log("Congratulations, you made your first successful API call!");
-    } else if (error !== null) {
-        console.log("Something went wrong with your first API call.");
-        console.log("Here's some debug information:");
-        console.log(CompileErrorReport(error));
+function begin() {
+    try {
+        let player1Token = null;
+        let player2Token = null;
+        PlayFabClient.LoginWithCustomID(player1LoginRequest, (err, player1) => {
+            player1Token = player1.data.EntityToken;
+            console.log("Player 1 Token: ");
+            console.log(player1Token);
+            PlayFabClient.LoginWithCustomID(player2LoginRequest, (err, player2) => {
+                player2Token = player2.data.EntityToken;
+                console.log("Player 2 Token: ");
+                console.log(player2Token);
+            });
+        });
+    }
+    catch(ex) {
+        console.log(ex);
     }
 }
 
-// This is a utility function we haven't put into the core SDK yet.  Feel free to use it.
-function CompileErrorReport(error) {
-    if (error == null)
-        return "";
-    var fullErrors = error.errorMessage;
-    for (var paramName in error.errorDetails)
-        for (var msgIdx in error.errorDetails[paramName])
-            fullErrors += "\n" + paramName + ": " + error.errorDetails[paramName][msgIdx];
-    return fullErrors;
-}
-
-// Kick off the actual login call
-DoExampleLoginWithCustomID();
+begin();
